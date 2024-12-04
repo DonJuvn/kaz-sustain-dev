680475f4922eacb08d03309154f94e04c50718c4
DonJuan <armvnov1ch@gmail.com>
2024-12-05 01:09:10 +0500

    sight 2

--- src/pages/Guide.jsx
+++ src/pages/Guide.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import BgGuide from "../components/bg-guide";
+import Sight2 from "../components/sight2";
 
 const PageTwo = () => {
    return (
      <>
      <BgGuide />
+     <Sight2 />
      </>
    );
 };
