95d116c67039d76daf45e8d38a01eb5696734e36
DonJuan <armvnov1ch@gmail.com>
2024-12-05 01:27:31 +0500

    chronology

---
+++ src/components/chronology.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+const Chronology = () => {
+   return (
+      <div id="chronology">
+         <div className="container">
+            <div className="chronology">
+               <div className="item">
+                  <h1 id="title">
+                     <span>1992 г.</span>
+                  </h1>
+                  <p>
+                     1992 г. – окончил Университет Дружбы Народов им. П. Лумумбы
+                     в Москве, специализация – математика.
+                  </p>
+               </div>
+               <div className="item">
+                  <h1 id="title">
+                     <span>1992 - 1993 г.</span>
+                  </h1>
+                  <p>Директор ТОО «Салым-Актау».</p>
+               </div>
+               <div className="item">
+                  <h1 id="title">
+                     <span>1993 - 1994 г.</span>
+                  </h1>
+                  <p>Финансовый директор ТОО «Далель».</p>
+               </div>
+               <div className="item">
+                  <h1 id="title">
+                     <span>1994 - 1997 г.</span>
+                  </h1>
+                  <p>Директор ТОО «Мунай».</p>
+               </div>
+               <div className="item">
+                  <h1 id="title">
+                     <span>1997 - 1998 г.</span>
+                  </h1>
+                  <p>Генеральный директор ТОО СП «Oil Co».</p>
+               </div>
+               <div className="item">
+                  <h1 id="title">
+                     <span>1998 - 1999 г.</span>
+                  </h1>
+                  <p>Генеральный директор АО «АктауТрансГаз».</p>
+               </div>
+               <div className="item">
+                  <h1 id="title">
+                     <span>1999 - 2000 г.</span>
+                  </h1>
+                  <p>Директор ТОО «Мунай».</p>
+               </div>
+               <div className="item">
+                  <h1 id="title">
+                     <span>2000 - 2004 г.</span>
+                  </h1>
+                  <p>
+                     Президент АО «МПК – РГП «Мангистауский Атомный
+                     Энергокомбинат».
+                  </p>
+               </div>
+               <div className="item">
+                  <h1 id="title">
+                     <span>2008 - 2024 гг.</span>
+                  </h1>
+                  <p>
+                     Председатель ОЮЛ и ИП Союза «Группа компаний ИНЖИНИРИНГ».
+                  </p>
+               </div>
+               <div className="item">
+                  <h1 id="title">
+                     <span>С 2024 года</span>
+                  </h1>
+                  <p>
+                     Председатель ОЮЛ и ИП Союза «Казахский институт ESG и
+                     Устойчивого Развития».
+                  </p>
+               </div>
+            </div>
+         </div>
+      </div>
+   );
+};
+
+export default Chronology;
--- src/pages/Guide.jsx
+++ src/pages/Guide.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import BgGuide from "../components/bg-guide";
 import Sight2 from "../components/sight2";
+import Chronology from "../components/chronology";
 
 const PageTwo = () => {
    return (
      <>
      <BgGuide />
      <Sight2 />
+     <Chronology />
      </>
    );
 };
