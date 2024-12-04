22b614927022810719d536b91ca2b8e723d1950b
DonJuan <armvnov1ch@gmail.com>
2024-12-05 01:58:18 +0500

    services

--- src/pages/Activity.jsx
+++ src/pages/Activity.jsx
@@ -1,11 +1,13 @@
 import React from "react";import BgActivity from "../components/bg-activity";
 import ActivityText from "../components/activity-text";
+import Services from "../components/services";
 
 const Activity = () => {
    return (
       <>
          <BgActivity />
          <ActivityText />
+         <Services />
       </>
    );
 };
