0d8f44ba1ab8760fe8fdc6ab4dbd328f601b9d73
DonJuan <armvnov1ch@gmail.com>
2024-12-04 20:01:50 +0500

    potential

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import Menu from "./components/menu";
 import About from "./components/about";
+import Potential from "./components/potential";
 
 function App() {
    return (
       <div className="App">
          <Menu />
          <About />
+         <Potential />
       </div>
    );
 }
