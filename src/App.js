10a08f98fb789586be8a7458339ea9bdc5e8a28c
DonJuan <armvnov1ch@gmail.com>
2024-12-04 19:23:47 +0500

    about block

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Menu from "./components/menu";
+import About from "./components/about";
 
 function App() {
    return (
       <div className="App">
          <Menu />
+         <About />
       </div>
    );
 }
---
+++ src/components/about.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const About = () => {
+   return(
+      <div id="about">
+         <div className="container">
+            <div className="about">
+               <div className="text">
+                  <div className="inner">
+                     <h1 id="title"><span>Казахский институт ESG</span> <br/>и устойчивого развития</h1>
+                     <p>Создаём устойчивое будущее для Казахстана, объединяя лидеров и идеи для заботы о планете и обществе. Вместе к процветанию и стабильности!</p>
+                  </div>
+                  <div className="links">
+                     <a href="#">Whatsapp<img src="img/link.png" alt="" /></a>
+                     <a href="#">Linked in<img src="img/link.png" alt="" /></a>
+                     <a href="#">Instagram<img src="img/link.png" alt="" /></a>
+                  </div>
+               </div>
+               <div className="img">
+                  <img src="img/bg.png" alt="" />
+               </div>
+            </div>
+         </div>
+      </div>
+   )
+}
+
+export default About;
