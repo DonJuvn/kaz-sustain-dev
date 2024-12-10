25f33a8ee3860a00c8699cc11323442af7135bc2
DonJuan <armvnov1ch@gmail.com>
2024-12-10 13:35:45 +0500

    responsive details

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,26 +1,40 @@
 import React from "react";
-
 const About = () => {
-   return(
+   return (
       <div id="about">
          <div className="container">
             <div className="about">
                <div className="text">
                   <div className="inner">
-                     <h1 id="title"><span>Казахский институт ESG</span> <br/>и устойчивого развития</h1>
-                     <p>Создаём устойчивое будущее для Казахстана, объединяя лидеров и идеи для заботы о планете и обществе. Вместе к процветанию и стабильности!</p>
+                     <h1 id="title">
+                        <span>Казахский институт ESG</span> <br />и устойчивого
+                        развития
+                     </h1>
+                     <p>
+                        Создаём устойчивое будущее для Казахстана, объединяя
+                        лидеров и идеи для заботы о планете и обществе. Вместе к
+                        процветанию и стабильности!
+                     </p>
                   </div>
                   <div className="links">
-                     <a href="#">Whatsapp<img src="img/link.png" alt="" /></a>
-                     <a href="#">Linked in<img src="img/link.png" alt="" /></a>
-                     <a href="#">Instagram<img src="img/link.png" alt="" /></a>
+                     <a href="#">
+                        Whatsapp
+                        <img src="img/link.svg" alt="" />
+                     </a>
+                     <a href="#">
+                        Linked in
+                        <img src="img/link.svg" alt="" />
+                     </a>
+                     <a href="#">
+                        Instagram
+                        <img src="img/link.svg" alt="" />
+                     </a>
                   </div>
                </div>
-               
             </div>
          </div>
       </div>
-   )
-}
+   );
+};
 
 export default About;
